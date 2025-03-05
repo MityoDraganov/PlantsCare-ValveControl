35d8ae2f9106579a45079e094817483e2a0033b2
Mityo Draganov <mityodraganow@gmail.com>
2025-03-05 16:27:44 +0200

    updated interfaces

--- ValveControl.cpp
+++ ValveControl.cpp
@@ -1,8 +1,11 @@
 #include "ValveControl.h"
+#include "../ControlManager/ControlManager.h"
+
+static ValveControl valveControl;
 
 ValveControl::ValveControl() : dependentSensorSerial("12345")  // Constructor with default sensor serial
 {
-    // Initialize any other members if necessary
+    ControlManager::registerControl(this); 
 }
 
 void ValveControl::init(int gpioPin)
@@ -12,17 +15,44 @@ void ValveControl::init(int gpioPin)
     digitalWrite(gpio, RELAY_OFF);  // Start with the relay off (set the pin to HIGH)
 }
 
-int ValveControl::getONValue() const
-{
-    return RELAY_ON;  // Low value to activate the relay
-}
-
-int ValveControl::getOFFValue() const
-{
-    return RELAY_OFF;  // High value to deactivate the relay
-}
-
 const std::string &ValveControl::getDependentSensor() const
 {
     return dependentSensorSerial;  // Return the serial number of the dependent sensor
+}
+
+
+void ValveControl::start()
+{
+    digitalWrite(gpio, RELAY_ON);  // Activate the relay (set the pin to LOW)
+}
+
+void ValveControl::stop()
+{
+    digitalWrite(gpio, RELAY_OFF);  // Deactivate the relay (set the pin to HIGH)
+}
+
+void ValveControl::setMinValue(int minValue)
+{
+    this->minValue = minValue;  // Set the minimum value
+}
+
+int ValveControl::getMinValue() const
+{
+    return minValue;  // Return the minimum value
+}
+
+void ValveControl::setMaxValue(int maxValue)
+{
+    this->maxValue = maxValue;  // Set the maximum value
+}
+
+int ValveControl::getMaxValue() const
+{
+    return maxValue;  // Return the maximum value
+}
+
+const std::string &ValveControl::getType() const
+{
+    static const std::string type = "ValveControl";  // Return the type of the control
+    return type;
 }
--- ValveControl.h
+++ ValveControl.h
@@ -9,17 +9,28 @@ class ValveControl : public Control
 public:
     ValveControl();  // Constructor declaration
 
+    void setMinValue(int minValue) override;  // Set the minimum value for the control
+    void setMaxValue(int maxValue) override;  // Set the maximum value for the control
+
+    void start() override;  // Start the valve control
+    void stop() override;   // Stop the valve control
+
     void init(int gpioPin) override;         // Initialize the valve control (GPIO pin setup)
-    int getONValue() const override;         // Return the ON value for the control (low level signal)
-    int getOFFValue() const override;        // Return the OFF value for the control (high level signal)
+    int getMinValue() const override;         // Return the ON value for the control (low level signal)
+    int getMaxValue() const override;        // Return the OFF value for the control (high level signal)
     const std::string &getDependentSensor() const override;  // Return the sensor's serial number
 
     // Optionally, you can add more functions to control the valve
+
+    const std::string& getType() const override;
+
 
 private:
     static const int RELAY_ON = LOW;    // Low signal activates the relay (logic 0)
     static const int RELAY_OFF = HIGH;   // High signal deactivates the relay (logic 1)
     std::string dependentSensorSerial;   // Serial number of the dependent sensor (could be a unique ID or other)
+    int minValue;                        // Minimum value for the control
+    int maxValue;                        // Maximum value for the control
 };
 
 #endif // VALVECONTROL_H
